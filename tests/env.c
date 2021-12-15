ed8ea64b80efe052313d677e3fff1a49c9eed18d
fbonini- <fbonini@student.42.fr>
2021-12-15 18:13:26 -0300

    tolken test

--- tests/env.c
+++ tests/env.c
@@ -6,7 +6,7 @@
 /*   By: fbonini <fbonini@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/08 18:28:54 by fbonini-          #+#    #+#             */
-/*   Updated: 2021/12/13 18:25:02 by fbonini          ###   ########.fr       */
+/*   Updated: 2021/12/15 18:12:57 by fbonini          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,7 +57,6 @@ t_env	*ft_alloc_env(char *envp)
 	// if (!env->content)
 	// {
 		// Free Error msg
-	// }
 	strncpy(env->content, &envp[key_size + 1], content_size); // Using STRING.H
 	env->next = env;
 	env->prev = env;
@@ -135,10 +134,10 @@ void	print_env(t_env_list *env_list, char **envp)
 	printf("The key is: %s	the content is: %s\n", env->key, env->content);
 	printf("\n\n------Get PWD--------\n");
 	total = env_list->total;
-	env = env_list->first;
+	env = env_list->last;
 	while (total != 0)
 	{
-		if (strncmp(env->key,"PWD", 3) == 0)  // Using STRING.H
+		// if (strncmp(env->key,"PWD", 3) == 0)  // Using STRING.H
 			printf("%s			%s\n", env->key, env->content);
 		env = env->next;
 		total--;
@@ -150,15 +149,178 @@ void	print_env(t_env_list *env_list, char **envp)
 
 
 /* ---------------------------------------------     --------------------------------------------- */
-/* ------------------------------------------- Parse --------------------------------------------- */
+/* ---------------------------------------- Token List ------------------------------------------ */
 /* ---------------------------------------------     --------------------------------------------- */
 
-// int	ft_parse_input(char *input)
-// {
+void	print_tolken(t_tolken_list *tolken_list)
+{
+	t_tolken	*tolken;
+	int		total;
+
+	tolken = NULL;
+	tolken = tolken_list->last;
+	total = tolken_list->total;
+	tolken = tolken_list->last;
+	while (total != 0)
+	{
+	// 	// if (strncmp(env->key,"PWD", 3) == 0)  // Using STRING.H
+		printf("The I is %d The Size is %d\n", total, tolken_list->total);
+		tolken = tolken->next;
+		total--;
+	}
+}
+
+t_tolken_list	*ft_alloc_tolken_list(void)
+{
+	t_tolken_list	*tolken_list;
 	
-// }
+	tolken_list = (t_tolken_list *) malloc (sizeof(t_tolken_list));
+	// if (!env_list)
+	// {
+		// Free Error msg
+	// }
+	tolken_list->first = NULL;
+	tolken_list->last = NULL;
+	tolken_list->total = 0;
+	return (tolken_list);
+}
+
+t_tolken	*ft_alloc_tolken(char *input)
+{
+	t_tolken	*tolken;
+	int			i;
+	int			key;
+	int			content;
+
+	i = 0;
+	key = 0;
+	content = 0;
+	while(input[i] == ' ')
+	{
+		i++;
+		// printf("Aqui\n");
+	}
+	while (input[i + key] != ' ')
+	{
+		key++;
+	}
+	while (input[i + key + content + 1] != '\0')
+	{
+		if (input[i + key + content + 1] == '"')
+		{
+			while (input[i + key + content + 1] != '"')
+				content++;
+		}
+		if (input[i + key + content + 1] == '\'')
+		{
+			while (input[i + key + content + 1] != '\'')
+				content++;
+		}
+		if (input[i + key + content + 1] == '|')
+			break;
+		content++;
+	}
+	tolken = (t_tolken *) malloc (sizeof(t_tolken));
+	tolken->key = (char *) malloc ((key + 1) * sizeof(char));
+	strncpy(tolken->key, &input[i], key);// Using STRING.
+	tolken->content = (char *) malloc ((content + 1) * sizeof(char));
+	strncpy(tolken->content, &input[i + key + 1], content); // Using STRING.H
+	tolken->size = key + content;
+	tolken->next = tolken;
+	tolken->prev = tolken;
+	return (tolken);
+}
+
+// input[0]
+// &input[| + 1]
+// &input[| + 1]
+
+void	ft_fill_tolken_list(t_tolken_list *tolken_list, char *input)
+{
+	t_tolken	*tolken;
+	int			i;
+
+	i = 0;
+	while (input[i] != '\0')
+	{
+		tolken = ft_alloc_tolken(&input[i]);
+		if (tolken_list->total == 0)
+			tolken_list->last = tolken;
+		else
+		{
+			tolken->next = tolken_list->first;
+			tolken_list->first->prev = tolken;
+			tolken->prev = tolken_list->last;
+			tolken_list->last->next = tolken;
+		}
+		tolken_list->first = tolken;
+		tolken_list->total++;
+		printf("The Key is %s The Content is %s\n", tolken_list->first->key, tolken_list->first->content);
+		i = i + tolken_list->first->size + 2;
+	}
+}
+
+void	ft_free_tolken(t_tolken_list *tolken_list, t_tolken *tolken)
+{
+	if (tolken_list->first == tolken)
+	{
+		if (tolken_list->last == tolken)
+		{
+			tolken_list->first = NULL;
+			tolken_list->last = NULL;
+		}
+		else
+		{
+			tolken_list->first = tolken->next;
+			tolken_list->last->next = tolken->next;
+			tolken->next->prev = tolken_list->last;
+		}
+	}
+	else
+	{
+		if (tolken_list->last == tolken)
+			tolken_list->last = tolken->prev;
+		tolken->prev->next = tolken->next;
+		tolken->next->prev = tolken->prev;
+	}
+	tolken_list->total--;
+	free(tolken);
+}
+
+void	ft_free_tolken_list(t_tolken_list *tolken_list)
+{
+	while(tolken_list->total != 0)
+	{
+		tolken_list->first->size = 0;
+		free(tolken_list->first->key);
+		free(tolken_list->first->content);
+		ft_free_tolken(tolken_list,tolken_list->first);
+	}
+}
+
+
 /* ---------------------------------------------     --------------------------------------------- */
-/* ------------------------------------------- Parse --------------------------------------------- */
+/* ---------------------------------------- Token List ------------------------------------------ */
+/* ---------------------------------------------     --------------------------------------------- */
+
+
+/* ---------------------------------------------     --------------------------------------------- */
+/* ---------------------------------------- Token Split ------------------------------------------ */
+/* ---------------------------------------------     --------------------------------------------- */
+
+// void	ft_create_tolken(t_mem *mem, char *input)
+// {
+// 	int	i;
+
+// 	// Key
+// 	while(input[i] != ' ')
+// 	{
+		
+// 	}
+// }
+//a
+/* ---------------------------------------------     --------------------------------------------- */
+/* ---------------------------------------- Token Split ------------------------------------------ */
 /* ---------------------------------------------     --------------------------------------------- */
 
 
@@ -239,7 +401,7 @@ void	ft_read_input(char **input)
 	free(prompt);
 }
 
-void	ft_create_shell(void)
+void	ft_create_shell(t_mem *mem)
 {
 	char	*input;
 	// int		parse;
@@ -249,10 +411,16 @@ void	ft_create_shell(void)
 		input = NULL;
 		ft_read_input(&input);
 		ft_create_history(input);
+		ft_fill_tolken_list(mem->tolken_list, input);
+		// print_tolken(mem->tolken_list);
+		// ft_create_tolken(mem, input);
 		// parse = ft_parse_input(input);
 		printf("The input was:\n%s\n", input);
 		if (input)
+		{
+			ft_free_tolken_list(mem->tolken_list);
 			free(input);
+		}
 	}
 	rl_clear_history();
 }
@@ -268,7 +436,8 @@ void	ft_create_shell(void)
 
 void	ft_alloc_mem(t_mem *mem)
 {
-	mem->env_list = ft_alloc_env_list();
+	// mem->env_list = ft_alloc_env_list();
+	mem->tolken_list = ft_alloc_tolken_list();
 }
 
 /* ---------------------------------------------     --------------------------------------------- */
@@ -284,8 +453,8 @@ int	main(int argc, char **argv, char **envp)
 	(void) envp;
 	ft_alloc_mem(&mem);
 	// ft_fill_env_list(mem.env_list,envp);
-	// // print_env(mem.env_list, envp);
+	// print_env(mem.env_list, envp);
 	// ft_free_env_list(mem.env_list);
-	ft_create_shell();
+	ft_create_shell(&mem);
 	return (0);
 }
--- tests/test.h
+++ tests/test.h
@@ -24,24 +24,42 @@ typedef struct s_env_list
 	unsigned int	total;
 }			t_env_list;
 
-typedef struct s_parse
-{
-	struct s_env	*next;
-	struct s_env	*prev;
-	char			*parsed;
-}			t_parse;
 
-typedef struct s_parse_list
+typedef struct s_tolken
 {
-	t_env			*first;
-	t_env			*last;
-	unsigned int	total;
-}			t_parse_list;
+	struct s_tolken	*next;
+	struct s_tolken	*prev;
+	char			*key;
+	char			*content;
+	int				size;
+}			t_tolken;
+
+typedef struct s_tolken_list
+{
+	t_tolken			*first;
+	t_tolken			*last;
+	unsigned int		total;
+}			t_tolken_list;
+
+
+// typedef struct s_parse
+// {
+// 	struct s_env	*next;
+// 	struct s_env	*prev;
+// 	char			*parsed;
+// }			t_parse;
+
+// typedef struct s_parse_list
+// {
+// 	t_env			*first;
+// 	t_env			*last;
+// 	unsigned int	total;
+// }			t_parse_list;
 
 typedef struct s_mem
 {
-	t_env_list		*env_list;
-	t_parse_list	*parse_list;
+	// t_env_list		*env_list;
+	t_tolken_list	*tolken_list;
 }			t_mem;
 
 #endif
